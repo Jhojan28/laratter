daf609f1e4ce9c2f6cd9500ce5ed323248dd5287
Jhojan Garcia <jhojanestiven1996@gmail.com>
2018-09-05 22:20:00 -0500

    new functionalities, follows and unfollows

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class)->times(50)->create()
-            ->each(function(App\User $user){
-                factory(App\Message::class)
-                ->times(20)
-                ->create([
-                    'user_id' => $user->id
-                ]);
+        $users = factory(App\User::class)->times(50)->create();
+
+        $users->each(function(App\User $user) use ($users){
+            factory(App\Message::class)
+            ->times(20)
+            ->create([
+                'user_id' => $user->id
+            ]);
+
+            $user->follows()->sync($users->random(10));
         });
         
     }
